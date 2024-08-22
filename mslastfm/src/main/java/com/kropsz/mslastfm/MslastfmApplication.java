b66f62f149861fe847f916e987c4b50c1ae0fdae
Pedro Esteves <kropinho211@outlook.com>
2024-08-22 20:54:52 -0300

    feat: add mslastfm project

---
+++ mslastfm/src/main/java/com/kropsz/mslastfm/MslastfmApplication.java
@@ -0,0 +1,13 @@
+package com.kropsz.mslastfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslastfmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslastfmApplication.class, args);
+	}
+
+}
---
+++ mslastfm/src/test/java/com/kropsz/mslastfm/MslastfmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kropsz.mslastfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MslastfmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
