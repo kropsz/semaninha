98a0fd2a6f159fda390d64b7b326b52ebe878b56
Pedro Esteves <kropinho211@outlook.com>
2024-08-23 11:25:47 -0300

    feat: endpoint to create and get collages

---
+++ mslastfm/src/main/java/com/kropsz/mslastfm/controller/LastFmController.java
@@ -0,0 +1,41 @@
+package com.kropsz.mslastfm.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kropsz.mslastfm.data.model.Collage;
+import com.kropsz.mslastfm.dto.Request;
+import com.kropsz.mslastfm.service.lastfm.CollageService;
+import com.kropsz.mslastfm.service.user.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/v1/semaninha")
+@RequiredArgsConstructor
+public class LastFmController {
+
+    private final CollageService collageService;
+    private final UserService userService;
+
+    @PostMapping("/collage")
+    public ResponseEntity<String> createCollage(@RequestBody @Valid Request request) throws IOException {
+        collageService.createCollage(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Collage created!!");
+    }
+
+    @GetMapping("/collage/{username}")
+    public ResponseEntity<List<Collage>> getCollageByUser(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getCollages(username));
+    }
+}
