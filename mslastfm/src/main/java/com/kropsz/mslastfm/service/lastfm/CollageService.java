22cd85da54dc0119f410268d3743b8b55b91bfdc
Pedro Esteves <kropinho211@outlook.com>
2024-08-23 10:20:58 -0300

    feat: CollageService added to service layer

---
+++ mslastfm/src/main/java/com/kropsz/mslastfm/service/lastfm/CollageService.java
@@ -0,0 +1,48 @@
+package com.kropsz.mslastfm.service.lastfm;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import org.springframework.stereotype.Service;
+
+import com.kropsz.mslastfm.data.model.UserData;
+import com.kropsz.mslastfm.dto.Request;
+import com.kropsz.mslastfm.feign.LastfmClient;
+import com.kropsz.mslastfm.service.s3.S3Service;
+import com.kropsz.mslastfm.service.user.UserService;
+import com.kropsz.mslastfm.util.CollageConstructor;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class CollageService {
+
+    private final LastfmClient lastfmClient;
+    private final CollageConstructor collageConstructor;
+    private final S3Service s3Service;
+    private final UserService userService;
+
+    public void createCollage(Request request) throws IOException {
+        var user = userService.getUserData(request.user());
+
+        var response = lastfmClient.getTopAlbums(request);
+        var image =  collageConstructor.drawImagesInGrid(response, request.limit());
+        saveCollage(image, user);
+    }
+
+    public void saveCollage(BufferedImage image, UserData user) throws IOException {
+        String fileName = "collage_" + user.getUser() + "_" + user.getCollages().size() + ".png";
+        s3Service.uploadImage(image, fileName);
+        userService.addCollageToUser(user, getCollageUrl(fileName));
+
+    }
+
+    public URL getCollageUrl(String fileName){
+        return s3Service.getPublicUrl(fileName);
+    }
+
+
+}
