21d4492a130e3b822c92551bd8f724c99170ef97
Pedro Esteves <kropinho211@outlook.com>
2024-08-23 09:11:12 -0300

    feat: UserService added to save, search and get collages

--- mslastfm/src/main/java/com/kropsz/mslastfm/data/repository/UserDataRepository.java
+++ mslastfm/src/main/java/com/kropsz/mslastfm/data/repository/UserDataRepository.java
@@ -1,9 +1,13 @@
 package com.kropsz.mslastfm.data.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.kropsz.mslastfm.data.model.UserData;
 
 public interface UserDataRepository extends MongoRepository<UserData, String> {
+
+    Optional<UserData> findByUser(String username);
 
 }
---
+++ mslastfm/src/main/java/com/kropsz/mslastfm/service/user/UserService.java
@@ -0,0 +1,49 @@
+package com.kropsz.mslastfm.service.user;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kropsz.mslastfm.data.model.Collage;
+import com.kropsz.mslastfm.data.model.UserData;
+import com.kropsz.mslastfm.data.repository.UserDataRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserDataRepository userRepository;
+
+    public UserData getUserData(String username) {
+        return userRepository.findByUser(username)
+                .orElseGet(() -> createNewUserData(username));
+    }
+
+    private UserData createNewUserData(String username) {
+        return new UserData(username);
+    }
+
+    public void save(UserData user) {
+        userRepository.save(user);
+    }
+
+    public void addCollageToUser(UserData user, URL fileName) {
+        Collage collage = new Collage(fileName, LocalDate.now());
+        user.addCollage(collage);
+        userRepository.save(user);
+    }
+
+    public List<Collage> getCollages(String username) {
+        var optionalUser = userRepository.findByUser(username);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get().getCollages();
+        }
+        return List.of();
+
+    }
+
+}
