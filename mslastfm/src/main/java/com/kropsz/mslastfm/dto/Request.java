3404ae8a5ac0a6b8afab35cf892908f3aa566d4c
Pedro Esteves <kropinho211@outlook.com>
2024-08-23 16:51:30 -0300

    refactor: transform Request record to class

--- mslastfm/src/main/java/com/kropsz/mslastfm/dto/Request.java
+++ mslastfm/src/main/java/com/kropsz/mslastfm/dto/Request.java
@@ -2,13 +2,21 @@ package com.kropsz.mslastfm.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record Request(String user, 
 
-    @Pattern(regexp = "overall|7day|1month", 
-             message = "period must be one of: overall, 7day, 1month")
-    String period, 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Request {
+
+    private String user;
+
+    @Pattern(regexp = "overall|7day|1month", message = "period must be one of: overall, 7day, 1month")
+    private String period;
+
     @Max(25)
-    int limit) {
-
+    private int limit;
 }
