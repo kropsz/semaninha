b2c99313fb701bf91145b3facaf8e2d86aee9da2
Pedro Esteves <kropinho211@outlook.com>
2024-08-23 08:55:13 -0300

    feat: feign client for lastfm api

---
+++ mslastfm/src/main/java/com/kropsz/mslastfm/feign/LastfmClient.java
@@ -0,0 +1,33 @@
+package com.kropsz.mslastfm.feign;
+
+import com.kropsz.mslastfm.config.LastFmKeyConfig;
+import com.kropsz.mslastfm.dto.Request;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.kropsz.mslastfm.dto.album.AlbumsResponse;
+import com.kropsz.mslastfm.dto.track.TrackResponse;
+
+@FeignClient(name = "LastFM", url = "https://ws.audioscrobbler.com/2.0", configuration = LastFmKeyConfig.class)
+public interface LastfmClient {
+
+        @GetMapping("/?method=user.gettopalbums&user={user}&period={period}&limit={limit}&format=json")
+        AlbumsResponse getTopAlbums(@PathVariable String user,
+                        @PathVariable String period,
+                        @PathVariable int limit);
+
+        @GetMapping("/?method=user.gettoptracks&user={user}&period={period}&limit={limit}&format=json")
+        TrackResponse getTopTracks(@PathVariable String user,
+                        @PathVariable String period,
+                        @PathVariable int limit);
+
+        default AlbumsResponse getTopAlbums(Request request) {
+                return getTopAlbums(request.user(), request.period(), request.limit());
+        }
+
+        default TrackResponse getTopTracks(Request request) {
+                return getTopTracks(request.user(), request.period(), request.limit());
+        }
+
+}
