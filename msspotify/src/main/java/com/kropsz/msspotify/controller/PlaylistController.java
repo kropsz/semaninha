f5542f1680388f9ad6930f9a8077f5bedefe7b91
Pedro Esteves <kropinho211@outlook.com>
2024-08-26 14:35:15 -0300

    feat: endpoint to create playlist

---
+++ msspotify/src/main/java/com/kropsz/msspotify/controller/PlaylistController.java
@@ -0,0 +1,30 @@
+package com.kropsz.msspotify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kropsz.msspotify.entity.UserDetails;
+import com.kropsz.msspotify.entity.enums.PlaylistType;
+import com.kropsz.msspotify.service.PlaylistService;
+import com.kropsz.msspotify.service.session.UserDetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/playlists")
+@RequiredArgsConstructor
+public class PlaylistController {
+
+    private final PlaylistService playlistService;
+    private final UserDetailsService userDetails;
+
+    @PostMapping("/create/{user}")
+    public ResponseEntity<String> getPlaylistLink(@PathVariable String user) {
+        UserDetails details = userDetails.getUserDetails();
+        return ResponseEntity.ok(playlistService.buildPlaylistWithTracks(details, user, PlaylistType.USER_TRACKS));
+    }
+
+}
