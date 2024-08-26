599a8243bdc7cf792d45e1c46982ccf5f7829108
Pedro Esteves <kropinho211@outlook.com>
2024-08-26 15:24:45 -0300

    config: create msrecommendation project

---
+++ msrecommendation/src/main/java/com/kropsz/msrecommendation/MsrecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.kropsz.msrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrecommendationApplication.class, args);
+	}
+
+}
