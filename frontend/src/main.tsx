5fb2caa42d6dcbc7e92aa213ccd949a20c61a18d
pedro esteves <kropinho211@outlook.com>
2024-09-09 14:44:56 -0300

    feat: add responsive design to the home page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './pages/Home'
+import Home from './pages/home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
