71fe484c0669dbcc7e5fb042fe68346a27b871e2
pedro esteves <kropinho211@outlook.com>
2024-09-11 13:57:53 -0300

    feat: added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './pages/home'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './pages/home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="nova-pagina" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
