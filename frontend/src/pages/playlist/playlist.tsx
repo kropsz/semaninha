3099f3c64ec96d307e0f3a4e3f92f4f2ef3f2e9b
Pedro Esteves <kropinho211@outlook.com>
2024-09-13 11:32:32 -0300

    feat: route call to authentificate with spotify and create playlist

--- frontend/src/pages/playlist/playlist.tsx
+++ frontend/src/pages/playlist/playlist.tsx
@@ -5,6 +5,7 @@ import { ChevronLeft } from 'lucide-react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Footer from '../../components/footer/footer';
 import axios from 'axios';
+import { useEffect } from 'react';
 
 const PlaylistComponent = () => {
   const navigate = useNavigate();
@@ -18,53 +19,73 @@ const PlaylistComponent = () => {
   const handleCreatePlaylist = async () => {
     try {
       const response = await axios.get('http://localhost:8082/api/login');
-      const authUrl = response.data; 
+      const authUrl = `${response.data}?redirect_uri=http://localhost:8082/api/get-user-code`;
       if (authUrl) {
-        window.open(authUrl, '_blank');
+        window.location.href = authUrl;
       }
     } catch (error) {
       console.error('Erro ao criar playlist:', error);
     }
   };
 
-  return <>
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.headers}>
-          <button onClick={handleBackClick} className={styles.backButton}>
-            <ChevronLeft className={styles.back} />
-          </button>
-          <h1 className={styles.title}>Sua Semaninha</h1>
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const isAuthenticated = params.get('authenticated');
+
+    if (isAuthenticated) {
+      const createPlaylist = async () => {
+        try {
+          await axios.post('http://localhost:8082/api/playlists/create/kropsky212');
+          console.log('Playlist criada com sucesso');
+        } catch (error) {
+          console.error('Erro ao criar playlist:', error);
+        }
+      };
+
+      createPlaylist();
+    }
+  }, [location.search]);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.headers}>
+            <button onClick={handleBackClick} className={styles.backButton}>
+              <ChevronLeft className={styles.back} />
+            </button>
+            <h1 className={styles.title}>Sua Semaninha</h1>
+          </div>
+          <p className={styles.description}>
+            Faça agora mesmo o Download da sua colagem e<br /> compartilhe com seus amigos!
+            <a href="link" className={styles.link} download> clique aqui!</a>
+          </p>
+          <div className={styles.spotify}>
+            <img
+              src={link}
+              alt="Album cover"
+              className={styles.image}
+            />
+            <img className={styles.seta} src={seta} alt="" />
+          </div>
         </div>
-        <p className={styles.description}>
-          Faça agora mesmo o Download da sua colagem e<br /> compartilhe com seus amigos!
-          <a href="link" className={styles.link} download> clique aqui!</a>
-        </p>
-        <div className={styles.spotify}>
+        <div className={styles.playlist}>
           <img
-            src={link}
-            alt="Album cover"
-            className={styles.image}
+            src={spotify}
+            alt="Spotify Icon"
+            className={styles.icon}
           />
-          <img className={styles.seta} src={seta} alt="" />
+          <p className={styles.playlistDescription}>
+            Se você gostou da colagem, crie agora mesmo a playlist no Spotify e aproveite ainda mais essa experiência
+          </p>
+          <button className={styles.button} onClick={handleCreatePlaylist}>
+            CRIAR PLAYLIST 🎶
+          </button>
         </div>
       </div>
- <div className={styles.playlist}>
-        <img
-          src={spotify}
-          alt="Spotify Icon"
-          className={styles.icon}
-        />
-        <p className={styles.playlistDescription}>
-          Se você gostou da colagem, crie agora mesmo a playlist no Spotify e aproveite ainda mais essa experiência
-        </p>
-        <button className={styles.button} onClick={handleCreatePlaylist}>
-          CRIAR PLAYLIST 🎶
-        </button>
-      </div>
-    </div>
-    <Footer />
-  </>;
+      <Footer />
+    </>
+  );
 };
 
 export default PlaylistComponent;
